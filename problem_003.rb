4ca97e7b46b6139c67cf265781db57eaadd05936
Matthew Hassfurder <sephonicus@gmail.com>
2013-02-23 02:11:12 -0800

    Primes up to a limit, determination of divisors in range could be useful.

--- problem_003.rb
+++ problem_003.rb
@@ -1,6 +1,34 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600_851_475_143 ?
 
+def has_divisors_in_range(n, min, max)
+  current = min
+  while current <= max
+    return true if n % current == 0
+    current += 1
+  end
+  false
+end
+
+def primes_up_to(n)
+  primes = []
+  return primes if n < 2
+  primes << 2
+  return primes if n == 3
+  primes << 3
+  return primes if n == 4
+
+  candidate = 3
+  while candidate < n
+    candidate += 1
+    next if primes.any? { |prime| candidate % prime == 0 }
+    max = (Math.sqrt(candidate) + 1).floor
+    next if has_divisors_in_range(candidate, primes.last + 1, max)
+    primes << candidate
+  end
+  primes
+end
+
 def prime?(number)
   if number > 3
     current = number - 1
@@ -30,7 +58,9 @@ def largest_prime_factor(n)
   largest
 end
 
-puts prime?(123481232).inspect
+# puts primes_up_to(-1).inspect
+
+# puts prime?(123481232).inspect
 
 # puts largest_prime_factor(13195) || 'None found'
 # puts largest_prime_factor(131952342) || 'None found'
