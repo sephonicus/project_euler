c6dc1349cbbe1bce104ac47166cddea8fb48f9d8
Matthew Hassfurder <sephonicus@gmail.com>
2013-02-23 01:02:35 -0800

    Better factorization, but determining if the largest candidate is prime would take over 5 minutes.

--- problem_003.rb
+++ problem_003.rb
@@ -2,7 +2,7 @@
 # What is the largest prime factor of the number 600_851_475_143 ?
 
 def prime?(number)
-  if number > 2
+  if number > 3
     current = number - 1
     while current > 1
       return false if number % current == 0
@@ -12,20 +12,26 @@ def prime?(number)
   true
 end
 
-def largest_prime_factor(number)
-  if number > 2
-    start = (number / 2.0).round
-    current = start
-    while current > 1
-      if number % current == 0
-        if prime?(current)
-          return current
-        end
+def largest_prime_factor(n)
+  largest = nil
+  if n > 3
+    current = 2
+    max = (Math.sqrt(n) + 1).floor
+    while current < max
+      if n % current == 0
+        factor = n / current
+        puts "#{current} and #{factor} are factors"
+        return factor if prime?(factor)
+        largest = current if prime?(current)
       end
-      current -= 1
+      current += 1
     end
   end
+  largest
 end
 
+puts prime?(123481232).inspect
+
 # puts largest_prime_factor(13195) || 'None found'
+# puts largest_prime_factor(131952342) || 'None found'
 # puts largest_prime_factor(600_851_475_143) || 'None found'
