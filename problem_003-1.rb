d14e725cf6a28f49f5f68511a8100db90de37b76
Matthew Hassfurder <sephonicus@gmail.com>
2013-02-25 17:31:50 -0800

    Notes for solution to problem 3. Starting second attempt.

---
+++ problem_003-1.rb
@@ -0,0 +1,15 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600_851_475_143 ?
+
+# Notes:
+
+# In number theory, the fundamental theorem of arithmetic, also called the
+# unique factorization theorem or the unique-prime-factorization theorem, states
+# that every integer greater than 1 is either prime itself or is the product of
+# prime numbers
+
+# Dividing a number by a relatively small number yields a larger number.
+# Dividing by a relatively large number yields a small number. The crossover
+# point is at the square root. Therefore, if a number cannot be divided by the
+# primes from 2 to its square root, it is prime itself.
+
