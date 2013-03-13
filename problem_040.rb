0a008008c7af99394909bd2963687373ff6ccd1b
Matthew Hassfurder <sephonicus@gmail.com>
2013-03-12 19:55:37 -0700

    Solution to problem 40.

--- problem_040.rb
+++ problem_040.rb
@@ -11,3 +11,19 @@
 # following expression.
 
 # d1 x d10 x d100 x d1000 x d10000 x d100000 x d1000000
+
+### Solution:
+
+fractional = ''
+counter = 1
+
+while fractional.size <= 1_000_000
+  fractional += counter.to_s
+  counter += 1
+end
+
+puts (0..6).to_a.map do |n|
+  10 ** n
+end.inject(1) do |m, n|
+  m = m * fractional[n - 1].to_i
+end
